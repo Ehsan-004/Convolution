0ae8bf58577d77d23874b0d94cb9ee5e5a5b3a46
Ehsan-004 <ehsankholusi@gmail.com>
2025-06-25 01:21:32 +0330

    Conv1D | works well

--- Conv1D.py
+++ Conv1D.py
@@ -4,13 +4,14 @@ import os
 
 
 l = 10
-m = ["#" for _ in range(l)]
+m = [1 for _ in range(l)]
 
 print(m)
 time.sleep(2)
 
 
 kernel_len = 3
+kernel = [2,2,2]
 start_index = 0
 
 max_index = len(m) - kernel_len + 1
@@ -18,9 +19,15 @@ max_index = len(m) - kernel_len + 1
 
 for i in range(max_index):
     # os.system("cls")
-    m = ["#" for _ in range(l)]
-    # m[i: kernel_len] = ["*" for _ in range(kernel_len)]
-    for j in range(i, i+ kernel_len):
-        m[j] = "0"
+    # m = [1 for _ in range(l)]
+    
+    for j in range(kernel_len):
+        m[i+j] *= kernel[j]
     pprint(m)
     time.sleep(1)
+
+print(m)
+
+
+def Conv1D(data: list, kernel: list):
+    pass
