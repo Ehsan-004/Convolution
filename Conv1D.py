3a6f8dec1deb66684141898a0290094b0efb77e8
Ehsan-004 <ehsankholusi@gmail.com>
2025-06-25 01:25:41 +0330

    Conv1D | converted to function

--- Conv1D.py
+++ Conv1D.py
@@ -3,31 +3,16 @@ import time
 import os
 
 
-l = 10
-m = [1 for _ in range(l)]
-
-print(m)
-time.sleep(2)
-
-
-kernel_len = 3
-kernel = [2,2,2]
-start_index = 0
-
-max_index = len(m) - kernel_len + 1
-
-
-for i in range(max_index):
-    # os.system("cls")
-    # m = [1 for _ in range(l)]
-    
-    for j in range(kernel_len):
-        m[i+j] *= kernel[j]
-    pprint(m)
-    time.sleep(1)
-
-print(m)
-
-
 def Conv1D(data: list, kernel: list):
-    pass
+    kernel_len = len(kernel)
+    max_index = len(data) - kernel_len + 1
+    for i in range(max_index):
+        for j in range(kernel_len):
+            data[i+j] *= kernel[j]
+    return data
+
+
+if __name__ == "__main__":
+    kernel = [2,2,2]
+    m = [1 for _ in range(10)]
+    pprint(Conv1D(m, kernel))
