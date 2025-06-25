6b64132f7c7404cf067db84862b03d04ed64795d
Ehsan-004 <ehsankholusi@gmail.com>
2025-06-25 15:19:54 +0330

    Conv1D | modularized the code

--- Conv1D.py
+++ Conv1D.py
@@ -3,10 +3,21 @@ import time
 import os
 
 
-def Conv1D(data: list, kernel: list):
+def Conv1D(data: list, kernel: list, stride: int = 1):
+    if len(kernel) > len(data):
+        raise ValueError("len(kernel) is larger than len(data)")
+    
+    if len(data) == 0 or len(kernel) == 0:
+        raise ValueError("kernel or data has length of 0 !")
+    
+    if stride < 1: 
+        raise ValueError("stride must be non-zero and pozitive")
+    
     kernel_len = len(kernel)
     max_index = len(data) - kernel_len + 1
-    for i in range(max_index):
+    for i in range(0, max_index, stride):
+        if i > len(data):
+            break
         for j in range(kernel_len):
             data[i+j] *= kernel[j]
     return data
@@ -14,5 +25,6 @@ def Conv1D(data: list, kernel: list):
 
 if __name__ == "__main__":
     kernel = [2,2,2]
-    m = [1 for _ in range(10)]
-    pprint(Conv1D(m, kernel))
+    for i in range(1, 15):
+        m = [1 for _ in range(10)]
+        pprint(Conv1D(m, kernel, i))
