cea4dcb4b44c7e77511de30da6b3fbd92f6f1303
Ehsan-004 <ehsankholusi@gmail.com>
2025-06-25 20:23:09 +0330

    Conv1D | feature map returns

--- Conv1D.py
+++ Conv1D.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import time
 import os
+from math import ceil
 
 
 def Conv1D(data: list, kernel: list, stride: int = 1, padding: int = 0, fill: int = 0):
@@ -14,19 +15,29 @@ def Conv1D(data: list, kernel: list, stride: int = 1, padding: int = 0, fill: in
         raise ValueError("stride must be non-zero and pozitive")
     
     data = [fill for i in range(padding)] + data + [fill for i in range(padding)]
-    
     kernel_len = len(kernel)
+    
+    feature_map = []
+       
     max_index = len(data) - kernel_len + 1
     for i in range(0, max_index, stride):
+        sum_ = 0
         if i > len(data):
             break
         for j in range(kernel_len):
-            data[i+j] *= kernel[j]
-    return data
+            tmp = data[i+j] * kernel[j]
+            data[i+j] = tmp
+            sum_ += tmp
+        feature_map.append(sum_)
+    return data, feature_map
 
 
 if __name__ == "__main__":
-    kernel = [2,2,2]
+    m = [2, 3, 4, 5, 1, 2]
+    kernel = [2,2]
     
-    m = [1 for _ in range(10)]
-    pprint(Conv1D(m, kernel, 2, padding=2, fill=3))
+    res = Conv1D(m, kernel, 2)
+    feature_map_len = ceil((len(m) - (len(kernel) - 1)) / (1))
+    print(feature_map_len)
+    pprint(res[0])
+    print(res[1])
