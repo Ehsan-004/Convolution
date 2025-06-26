5e9cbe71a94c3338dcf6829ae9a689c75509bad6
Ehsan-004 <ehsankholusi@gmail.com>
2025-06-26 18:11:02 +0330

    Conv2D | padding added

--- Conv2D.py
+++ Conv2D.py
@@ -6,26 +6,30 @@ from math import floor
 
 def Conv2D(data: list[list], kernel: list[list], stride: int = 1, padding: int = 0, fill: int = 0):
     
-    # kernel = [[2 for __ in range(kr)] for _ in range(kl)]
-    # data = [[1 for __ in range(r)] for _ in range(l)]
-    
     l = len(data)
     r = len(data[0])
     kl = len(kernel)
     kr = len(kernel[0])
     
+    if padding < 0:
+        raise ValueError(f"padding cannot be smaller than 0 but got {padding}")
+    elif padding > 0:
+        for i in range(0, l):
+            for _ in range(padding):
+                data[i].append(fill)
+        
+        for _ in range(padding):
+            data.append([fill for __ in range(r + padding)])
+        
+    l = len(data)
+    r = len(data[0])
+    
+    
     lines = floor((l - kl + 2 * padding) / stride) + 1
     rows = floor((r - kr + 2 * padding) / stride) + 1
     
-    print(f"lines: {lines} | rows: {rows}")
-    
     feature_map = [[[] for __ in range(rows)] for _ in range(lines)]
-    
-    print(feature_map)    
-    
-    print(f"i range: {list(range(0, lines, stride))}")
-    print(f"j range: {list(range(0, rows, stride))}")
-    
+        
     for i in range(0, lines):  # i corresponds to line
         for j in range(0, rows):  # j corresponds to row
             sum_ = 0
@@ -51,6 +55,7 @@ if __name__ == "__main__":
     pprint(m)
     time.sleep(1)
     os.system("cls")
-    r = Conv2D(m, kernel, 2)
+    r = Conv2D(m, kernel, 2, padding=2, fill=0)
+    
     pprint(r[0])
     pprint(r[1])
